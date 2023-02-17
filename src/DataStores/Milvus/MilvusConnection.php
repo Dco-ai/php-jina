1ba0774f278eecde8bea7398e5e3156fd2086ef9
Jonathan Rowley <jonathan@dco.ai>
2023-02-17 13:19:02 -0600

    Making PHPStan  corrections

--- src/DataStores/Milvus/MilvusConnection.php
+++ src/DataStores/Milvus/MilvusConnection.php
@@ -4,13 +4,6 @@ namespace DcoAi\PhpJina\DataStores\Milvus;
 
 class MilvusConnection
 {
-    private $conf;
-    private string $url;
-    public function __construct($conf) {
-        $this->conf = $conf;
-        $this->url = $this->conf["dataStore"]["url"].":".$this->conf["dataStore"]["port"];
-    }
-
     public function filter(): Filter
     {
         return new Filter;
--- src/DataStores/SQLite/SQLiteConnection.php
+++ src/DataStores/SQLite/SQLiteConnection.php
@@ -4,16 +4,8 @@ namespace DcoAi\PhpJina\DataStores\SQLite;
 
 class SQLiteConnection
 {
-    private $conf;
-    private string $url;
-    public function __construct($conf) {
-        $this->conf = $conf;
-        $this->url = $this->conf["dataStore"]["url"].":".$this->conf["dataStore"]["port"];
-    }
-
     public function filter(): Filter
     {
         return new Filter;
     }
-
 }
